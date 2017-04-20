4fca581b8d4896a82d974c45884e96db8d2ba498
Mikael Ångman <mikan972@student.liu.se>
2017-04-20 21:48:29 +0200

    wip backupscript

--- full_backup.sh
+++ full_backup.sh
@@ -2,29 +2,25 @@
 #The user doing the backup, needs access to all the files!
 BUSER = backup-bot
 #Location of the local backup
-BLOCATION = /home/$BUSER/Backups
+BLOCATION = /backup
 #Name of the backup
-OF=$BLOCATION/$(date +%Y%m%d%h%m)-d-server
+OF=$(date +%Y%m%d%h%m)-d-server
 #Number of backups to keep (= how old should the oldest be if run once each day)
 MAXAGE=7
 
 echo "Starting full system backup..."
 
 #The motherload....
-rsync -aAXvHS --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found",$BLOCATION} / $OF
+rsync -aAXvHS --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found",$BLOCATION} / $BLOCATION/$OF
 
 #Compress the backup
 tar -xZf $OF.tgz $OF
 
-#Remove backups older than a week
+#Remove the non-compressed folder
+rm -rf $BLOCATION/$OF
 
-#for i in $( ls ); do
-	#Get date from filename/file created
-	#file_date = .....
-	#age = $[$file_date - $(date +Y%m%d%h%m)] #file date - current date
-	#if [$age >= $MAXAGE]; then
-		rm -r i;
-	#fi
-#done
+#Remove backups older than MAXAGE
+find $BLOCATION -mtime +$MAXAGE -type f #-delete <-- Add this after testing!
+
 #Chmod-things
 
