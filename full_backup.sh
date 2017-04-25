45a30a666c11cd1ce7567a1aee9d21261539fd89
Mikael Ångman <mikaelangman@hotmail.com>
2017-04-25 16:51:27 +0200

    wip backup-script

--- full_backup.sh
+++ full_backup.sh
@@ -7,8 +7,28 @@ BLOCATION="/backup"
 OF=$(date +%Y%m%d%H%M)_d-server
 #Number of backups to keep (= how old should the oldest be if run once each day)
 MAXAGE=7
+#Log file location
+LOG="var/log/backup.log"
 
-echo "Starting full system backup..."
+#Output STDOUT & STDERR both to log-file and terminal
+exec > >(tee -ia /var/log/backup.log)
+exec 2> >(tee -ia /var/log/backup.log)
+
+echo "Starting full system backup at $(date ,%H:%M)"
+echo "Mounting /dev/vda1 to $BACKUP..."
+
+if grep -qs "$BLOCATION" /proc/mounts; then
+  echo "It's already mounted."
+else
+  mount "$mount"
+  if [ $? -eq 0 ]; then
+   echo "Mounted!"
+  else
+   echo "Something went wrong with the mount, exiting"
+   exit
+  fi
+fi
+
 mkdir $BLOCATION/$OF
 
 if [ ! -d "$BLOCATION/$OF" ]; then
@@ -18,18 +38,30 @@ fi
 echo "Backuplocation $BLOCATION/$OF created"
 #The motherload....
 echo "Starting rsync..."
-#rsync -aAXvHS --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","$BLOCATION/*"} / $BLOCATION/$OF
-touch /backup/$OF/hej.txt
+rsync -aAXvHS --exclude={"/var/cache/*","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","$BLOCATION/*"} / $BLOCATION/$OF
+
 echo "rsync done! Tar is next..."
+
 #Compress the backup
 cd $BLOCATION
 tar -zcf $OF.tgz $OF
 echo "Compressed and ready"
+
 #Remove the non-compressed folder
 rm -rf $BLOCATION/$OF
+echo "Raw data removed"
 
 #Remove backups older than MAXAGE
-find $BLOCATION -mtime +$MAXAGE -type f -delete
+#find $BLOCATION -mtime +$MAXAGE -type f -delete
+echo "Old backups removed"
 
 #Chmod-things
 chown backup-bot:admins $BLOCATION/$OF.tgz
+echo "Ownership established"
+
+#Unmount
+umount $BLOCATION
+echo "Unmounted $BLOCATION"
+echo "$(date +%H:%M)Done"
+
+exit
